ffcd593a3f705d6d2b1d1beee307ecdc657a2ff2
anliuke <1903417829@qq.com>
2020-12-19 17:06:55 +0800

    第一次提交

---
+++ example/App.vue
@@ -0,0 +1,38 @@
+<template>
+<section>
+  <vue-emoji ref="emoji" width="400" @input="onInput" :value="myText" />
+  <br>
+  You've entered: {{readText}}<br>
+  <button @click="clearTextarea()">Clear</button>
+</section>
+</template>
+
+<script>
+import VueEmoji from '../src/VueEmoji'
+
+export default {
+  name: 'app',
+  created() {
+    this.myText = window.localStorage.getItem('datas')
+  },
+  data(){
+    return {
+      myText: '',
+      readText: ''
+    }
+  },
+  methods: {
+    clearTextarea(){
+      this.$refs.emoji.clear()
+    },  
+    onInput(event){
+     console.log(event.data)
+     this.readText = event.data
+     window.localStorage.setItem('datas',event.data)
+    }
+  },
+  components: {
+    'vue-emoji': VueEmoji
+  }
+}
+</script>
